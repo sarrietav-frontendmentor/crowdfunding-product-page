d41b2d51ba200f370f4c2a1d5aab512e4b76ee3b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-27 23:10:12 -0500

    💄 Add header logo and menu image

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,18 @@ import React from 'react';
 
 const index = () => {
   return (
-    <header>
+    <header className="relative">
       <figure>
         <img src="/image-hero-mobile.jpg" alt="Apple computer" />
       </figure>
+      <div className="px-5 flex justify-between items-center absolute top-8 inset-x-0">
+        <figure>
+          <img src="/logo.svg" alt="Crowdfund logo" />
+        </figure>
+        <figure>
+          <img src="/icon-hamburger.svg" alt="Hamburger menu icon" />
+        </figure>
+      </div>
     </header>
   );
 };
