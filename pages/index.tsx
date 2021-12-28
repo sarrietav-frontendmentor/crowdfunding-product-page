25f8fe27718f362cfa2e26fdf9521f52bff8b51e
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-28 00:31:29 -0500

    💄 Add spacing between sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Section from '@/components/Section';
 import TheHeader from '@/components/TheHeader';
 import DataSection from '@/sections/DataSection';
 import ProjectSection from '@/sections/ProjectSection';
@@ -7,7 +6,7 @@ const index = () => {
   return (
     <div className="font-commissioner">
       <TheHeader />
-      <main className="px-6 relative -top-14">
+      <main className="px-6 relative -top-14 space-y-8">
         <ProjectSection />
         <DataSection />
       </main>
