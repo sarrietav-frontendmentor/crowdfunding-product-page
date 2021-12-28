51207569b5cb070d60ca3f670bc0a18b14eb5253
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-12-27 22:56:29 -0500

    💄 Add header image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
 
 const index = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header>
+      <figure>
+        <img src="/image-hero-mobile.jpg" alt="Apple computer" />
+      </figure>
+    </header>
+  );
+};
 
-export default index
+export default index;
